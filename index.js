e9aefd85fd398e4e0190b3bf759191b0def3a6fa
Redstone Zhao <13740080@qq.com>
2017-06-06 13:03:46 +0000

    全面 es6 化，全面拥抱 eslint/airbnb

--- index.js
+++ index.js
@@ -1,50 +1,40 @@
-var morgan            = require('morgan');
-var moment            = require('moment');
-var assert            = require('assert-plus');
-var FileStreamRotator = require('file-stream-rotator');
+const morgan = require('morgan');
+const moment = require('moment');
+const assert = require('assert-plus');
+const FileStreamRotator = require('file-stream-rotator');
 
+const defaultFormat = [
+  ':remote-addr - :remote-user [:date] ":method :url HTTP/:http-version"',
+  ':status :res[content-length] ":referrer" ":user-agent"',
+  ':response-time',
+].join(' ');
 
-module.exports = function(logpath, dateTimeFormat, logFormat) {
-  assert.string(logpath, 'logpath');
+module.exports = (_logpath, dateTimeFormat, _logFormat, immediate = false) => {
+  assert.string(_logpath, 'logpath');
   assert.string(dateTimeFormat, 'dateTimeFormat');
-  // format logPath
-  if (logpath.indexOf(".log") > -1) {
-    logpath = logpath.replace(/\.log$/gi, "-%DATE%.log");
-  } else {
-    logpath = `${logpath}-%DATE%.log`;
-  }
 
-  if (!logFormat) {
-    logFormat = [
-      ':remote-addr - :remote-user [:date] ":method :url HTTP/:http-version"',
-      ':status :res[content-length] ":referrer" ":user-agent"',
-      ':response-time'
-    ].join(' ');
+  let logpath;
+  const logFormat = _logFormat || defaultFormat;
+
+  // format logPath
+  if (_logpath.substr(-4) === '.log') {
+    logpath = `${_logpath.substring(0, _logpath.length - 4)}-%DATE%.log`;
+  } else {
+    logpath = `${_logpath}-%DATE%.log`;
   }
 
   // prepare a rotating write stream
-  var stream = FileStreamRotator.getStream({
+  const stream = FileStreamRotator.getStream({
     filename: logpath,
     frequency: 'daily',
     verbose: false,
-    date_format: "YYYYMMDD"
+    date_format: 'YYYYMMDD',
   });
 
-  morgan.token('remote-addr', function(req, res) {
-    return [req._clientIp, req._realIp, req._remoteIp].join(' - ');
-  });
-  morgan.token('remote-user', function(req, res) {
-    return req.user && req.user.id && '-';
-  });
-  morgan.token('date', function() {
-    return moment().format(dateTimeFormat);
-  });
-  morgan.token('req-body', function(req, res) {
-    return JSON.stringify(req.body);
-  });
+  morgan.token('remote-addr', (req) => [req._clientIp, req._realIp, req._remoteIp].join(' - '));
+  morgan.token('remote-user', (req) => req.user && req.user.id && '-');
+  morgan.token('date', () => moment().format(dateTimeFormat));
+  morgan.token('req-body', (req) => JSON.stringify(req.body));
 
-  return morgan(logFormat, { stream: stream });
+  return morgan(logFormat, { stream, immediate });
 };
-
-
-
---
+++ test/cases.js
@@ -0,0 +1,141 @@
+const fs = require('fs');
+const http = require('http');
+const moment = require('moment');
+const assert = require('assert');
+const middle = require('../');
+
+describe('open-rest-access-log', function testUnit() {
+  this.timeout(50 * 1000);
+  describe('noraml', () => {
+    it('case1', (done) => {
+      const date = moment().format('YYYYMMDD');
+      const logFile = `${__dirname}/access-${date}.log`;
+      if (fs.existsSync(logFile)) fs.unlinkSync(logFile);
+
+      const ware = middle(`${__dirname}/access`, 'YYYY-MM-DD HH:mm:ss', null, true);
+      assert.ok(ware instanceof Function);
+      const req = new http.ClientRequest();
+      Object.assign(req, {
+        _clientIp: '192.168.4.114',
+        _realIp: '192.168.4.191',
+        _remoteIp: '127.0.0.1',
+        user: {
+          id: 1,
+        },
+        headers: {
+          referrer: 'https://xiongfei.me/',
+        },
+        body: {
+          hello: 'world',
+        },
+      });
+      const res = new http.ServerResponse(req);
+
+      ware(req, res, (error) => {
+        res.end();
+        setTimeout(() => {
+          assert.ok(res.finished);
+          assert.equal(null, error);
+          const lines = fs.readFileSync(logFile).toString().trim().split('\n');
+          assert.equal(1, lines.length);
+          assert.ok(lines[0], '日志不为空');
+          assert.ok(lines[0].length > 30, '日志的长度肯定大于30');
+          assert.ok(lines[0].indexOf('192.168.4.114') > -1, '包含 _clientIp');
+          assert.ok(lines[0].indexOf('192.168.4.191') > -1, '包含 _realIp');
+          assert.ok(lines[0].indexOf('127.0.0.1') > -1, '包含 _remoteIp');
+
+          if (fs.existsSync(logFile)) fs.unlinkSync(logFile);
+          done();
+        }, 2000);
+      });
+    });
+
+    it('case2', (done) => {
+      const date = moment().format('YYYYMMDD');
+      const logFile = `${__dirname}/access-${date}.log`;
+      if (fs.existsSync(logFile)) fs.unlinkSync(logFile);
+
+      const logFormat = [
+        ':remote-addr - :remote-user [:date] ":method :url HTTP/:http-version"',
+        ':status :res[content-length] ":referrer" ":user-agent"',
+        ':req-body',
+      ].join(' ');
+      const ware = middle(`${__dirname}/access.log`, 'YYYY-MM-DD HH:mm:ss', logFormat, true);
+      assert.ok(ware instanceof Function);
+      const req = new http.ClientRequest();
+      Object.assign(req, {
+        _clientIp: '192.168.4.114',
+        _realIp: '192.168.4.191',
+        _remoteIp: '127.0.0.1',
+        user: {
+          id: 1,
+        },
+        headers: {
+          referrer: 'https://xiongfei.me/',
+        },
+        body: {
+          hello: 'world',
+        },
+      });
+      const res = new http.ServerResponse(req);
+
+      ware(req, res, (error) => {
+        res.end();
+        setTimeout(() => {
+          assert.ok(res.finished);
+          assert.equal(null, error);
+          const lines = fs.readFileSync(logFile).toString().trim().split('\n');
+          assert.equal(1, lines.length);
+          assert.ok(lines[0], '日志不为空');
+          assert.ok(lines[0].length > 30, '日志的长度肯定大于30');
+          assert.ok(lines[0].indexOf('world') > -1, '包含 body 的内容');
+          assert.ok(lines[0].indexOf('192.168.4.114') > -1, '包含 _clientIp');
+          assert.ok(lines[0].indexOf('192.168.4.191') > -1, '包含 _realIp');
+          assert.ok(lines[0].indexOf('127.0.0.1') > -1, '包含 _remoteIp');
+
+          if (fs.existsSync(logFile)) fs.unlinkSync(logFile);
+          done();
+        }, 2000);
+      });
+    });
+
+    it('case3', (done) => {
+      const date = moment().format('YYYYMMDD');
+      const logFile = `${__dirname}/access-${date}.log`;
+      if (fs.existsSync(logFile)) fs.unlinkSync(logFile);
+
+      const ware = middle(`${__dirname}/access.log`, 'YYYY-MM-DD HH:mm:ss');
+      assert.ok(ware instanceof Function);
+      const req = new http.ClientRequest();
+      Object.assign(req, {
+        _clientIp: '192.168.4.114',
+        _realIp: '192.168.4.191',
+        _remoteIp: '127.0.0.1',
+        user: {
+          id: 1,
+        },
+        headers: {
+          referrer: 'https://xiongfei.me/',
+        },
+        body: {
+          hello: 'world',
+        },
+      });
+      const res = new http.ServerResponse(req);
+
+      ware(req, res, (error) => {
+        res.end();
+        setTimeout(() => {
+          assert.ok(res.finished);
+          assert.equal(null, error);
+          const lines = fs.readFileSync(logFile).toString().trim().split('\n');
+          assert.equal(1, lines.length);
+          assert.equal(0, lines[0].length, '日志为空, 因为没有触发 res finish');
+
+          if (fs.existsSync(logFile)) fs.unlinkSync(logFile);
+          done();
+        }, 2000);
+      });
+    });
+  });
+});
